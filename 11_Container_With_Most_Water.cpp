78bbfc2689c3d23010da2388ff273997228ce871
yahyasungur <yahyasungur18@hotmail.com>
2024-10-02 11:20:24 +0300

    Problem 11 has been solved.

--- 11_Container_With_Most_Water.cpp
+++ 11_Container_With_Most_Water.cpp
@@ -3,7 +3,7 @@ LeetCode 11. Container With Most Water
 Problem Link: https://leetcode.com/problems/container-with-most-water
 
 Author: Yahya Sungur
-Date: 10/01/2024
+Date: 10/02/2024
 */
 
 #include <iostream>
@@ -17,34 +17,29 @@ public:
     int maxArea(vector<int>& height) 
     {
         int leftIndex = 0, rightIndex = height.size() -1;
-        int area = (rightIndex-leftIndex)*min(height[rightIndex], height[leftIndex]);
+        int area = 0;
         int tempArea = 0;
 
-        for (size_t i = 1; i < height.size(); i++)
+        while (true)
         {
-            // if (i > rightIndex || (height.size()-1-i) < leftIndex)
-            // {
-            //     break;
-            // }
-
-            if (height[i] > height[leftIndex])
+            tempArea = (rightIndex-leftIndex)*min(height[rightIndex], height[leftIndex]);
+            if (tempArea > area)
             {
-                tempArea = (rightIndex-i)*min(height[rightIndex], height[i]);
-                if (tempArea >= area)
-                {
-                    leftIndex = i;
-                    area = tempArea;
-                }
+                area = tempArea;
             }
-            
-            if (height[height.size()-1-i] > height[rightIndex])
+
+            if (height[leftIndex] < height[rightIndex])
             {
-                tempArea = ((height.size()-1-i)-leftIndex)*min(height[height.size()-1-i], height[leftIndex]);
-                if (tempArea >= area)
-                {
-                    rightIndex = height.size()-1-i;
-                    area = tempArea;
-                }
+                leftIndex++;
+            }
+            else
+            {
+                rightIndex--;
+            }
+
+            if (leftIndex >= rightIndex)
+            {
+                break;
             }
         }
 
@@ -56,7 +51,7 @@ int main()
 {
     Solution solution;
 
-    vector<int> heights = {2,3,4,5,18,17,6};
+    vector<int> heights = {1,8,6,2,5,4,8,3,7};
 
     int result = solution.maxArea(heights);
 
